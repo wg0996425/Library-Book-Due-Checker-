181f6fe0a74af6f24c58c7a5138437fd17d84a93
Viktor Gray <vg0996425@otc.edu>
2025-09-10 09:58:35 -0500

    added stuff. working on the functions.php

--- form.php
+++ form.php
@@ -1,1 +1,30 @@
 <!-- W. VIKTOR GRAY -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>FORM</title>
+</head>
+
+<body>
+    <form class="container mt-4">
+        <!-- RETURN DATE -->
+        <div class="mb-3">
+            <label for="returnDate" class="form-label">Return Date:</label>
+            <input type="date" class="form-control" id="returnDate" aria-describedby="returnDate" required>
+        </div>
+        <!-- DUE DATE -->
+        <div class="mb-3">
+            <label for="dueDate" class="form-label">Due Date:</label>
+            <input type="date" class="form-control" id="dueDate" required>
+        </div>
+        <!-- SUBMIT BUTTON -->
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+
+</html>
--- functions.php
+++ functions.php
@@ -1,1 +1,16 @@
 <!-- W. VIKTOR GRAY -->
+
+<?php
+    if ($_GET) {
+        $returnDate = $_GET['returnDate'];
+        $dueDate = $_GET['dueDate'];
+
+        $returnDate = strtotime($returnDate);
+        $dueDate = strtotime($dueDate);
+    }
+
+    function returnDate_strtodate() {
+        
+    }
+
+?>
